6adeacee104e258cb2dcaf1da36b0643afb4d27b
Adityaz <apple@Adityaz.local>
2025-11-11 13:58:48 +0530

    Added the theme and the fonts of our dorium app

---
+++ image.d.ts
@@ -0,0 +1,22 @@
+// The d in the name of the file of the code is known as decalration ->
+
+declare module "*png"{
+    const value: any;
+    export default value
+}
+declare module "*jpg"{
+    const value: any;
+    export default value
+}
+declare module "*gif"{
+    const value: any;
+    export default value
+}
+declare module "*svg"{
+    const value: any;
+    export default value
+}
+declare module "*jpeg"{
+    const value: any;
+    export default value
+}
